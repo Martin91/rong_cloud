9cf9417cc933fb2ab8f652fa2417935728efb45c
Martin Hong <hongzeqin@gmail.com>
2016-09-12 22:35:37 +0800

    update RongCloud::Configuration

--- lib/rong_cloud/configuration.rb
+++ lib/rong_cloud/configuration.rb
@@ -1,25 +1,24 @@
+require "logger"
+
 module RongCloud
   module Configuration
-    DEFAULT_CONFIGURATIONS = {
-      log_file: "log/rongcloud.log",
-      log_level: "DEBUG",
-      host: "https://api.cn.ronghub.com"
-    }
+    DEFAULT_HOST = "https://api.cn.ronghub.com".freeze
 
     module ModuleMethods
-      attr_accessor :app_key, :app_secret, :host, :log_file, :log_level
+      attr_accessor :app_key, :app_secret, :host, :logger
 
-      def self.extended(base)
-        base.singleton_class.instance_eval do
-          [:host, :log_file, :log_level].each do |config|
-            define_method config do
-              instance_variable_get("@#{config}") || DEFAULT_CONFIGURATIONS[config]
-            end
-          end
-        end
+      def host
+        @host || DEFAULT_HOST
+      end
+
+      def logger
+        @logger || default_logger
+      end
+
+      def default_logger
+        @default_logger ||= ::Logger.new(STDOUT)
       end
     end
-
     extend ModuleMethods
   end
 end
