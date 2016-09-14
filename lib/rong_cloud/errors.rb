7c56303c629f2c1425f4b3469a3b4422b6d04c55
Martin Hong <hongzeqin@gmail.com>
2016-09-14 09:02:03 +0800

    declare new error classes

---
+++ lib/rong_cloud/errors.rb
@@ -0,0 +1,25 @@
+module RongCloud
+  class RequestError < ::StandardError
+    attr_accessor :business_code
+  end
+
+  class BadRequest < RequestError;end
+  class AuthenticationFailed < RequestError;end
+  class RequestForbidden < RequestError;end
+  class ResourceNotFound < RequestError;end
+  class ExceedLimit < RequestError;end
+  class TooManyRequests < RequestError;end
+  class InternalServerError < RequestError;end
+  class Timeout < RequestError;end
+
+  HTTP_CODE_TO_ERRORS_MAP = {
+    "400" => BadRequest,
+    "401" => AuthenticationFailed,
+    "403" => RequestForbidden,
+    "404" => ResourceNotFound,
+    "405" => ExceedLimit,
+    "429" => TooManyRequests,
+    "500" => InternalServerError,
+    "504" => Timeout
+  }.freeze
+end
