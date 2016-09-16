e984be9672a497a618622443d23570126e32b50d
Martin Hong <hongzeqin@gmail.com>
2016-09-16 10:38:53 +0800

    refactor variable naming

--- lib/rong_cloud/services/user.rb
+++ lib/rong_cloud/services/user.rb
@@ -3,27 +3,27 @@ require 'rong_cloud/request'
 module RongCloud
   module Services
     module User
-      def get_token(userId, name, portraitUri)
-        request("/user/getToken", {userId: userId, name: name, portraitUri: portraitUri})
+      def get_token(user_id, name, portraitUri)
+        request("/user/getToken", {userId: user_id, name: name, portraitUri: portraitUri})
       end
 
-      def refresh_user(userId, name = nil, portraitUri = nil)
-        params = { userId: userId, name: name, portraitUri: portraitUri }
+      def refresh_user(user_id, name = nil, portraitUri = nil)
+        params = { userId: user_id, name: name, portraitUri: portraitUri }
         params.reject!{|key, value| value.nil?}
 
         request("/user/refresh", params)
       end
 
-      def check_online(userId)
-        request("/user/checkOnline", { userId: userId })
+      def check_online(user_id)
+        request("/user/checkOnline", { userId: user_id })
       end
 
-      def block_user(userId, minute)
-        request("/user/block", { userId: userId, minute: minute })
+      def block_user(user_id, minute)
+        request("/user/block", { userId: user_id, minute: minute })
       end
 
-      def unblock_user(userId)
-        request("/user/unblock", userId: userId)
+      def unblock_user(user_id)
+        request("/user/unblock", userId: user_id)
       end
 
       def blocked_users
