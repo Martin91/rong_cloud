9e06642ec298b02ce97ee0ba9f7e8e8381472cc2
Martin Hong <hongzeqin@gmail.com>
2016-09-16 10:10:16 +0800

    test check_online method

--- test/rong_cloud/services/user_test.rb
+++ test/rong_cloud/services/user_test.rb
@@ -1,7 +1,7 @@
 module RongCloud
   module Services
     module UserTest
-      def test_get_token_without_userId
+      def test_get_token_without_user_id
         error = assert_raises RongCloud::BadRequest do
           @service.get_token(nil, nil, nil)
         end
@@ -29,7 +29,7 @@ module RongCloud
         assert response["token"]
       end
 
-      def test_refresh_user_without_userId
+      def test_refresh_user_without_user_id
         error = assert_raises RongCloud::BadRequest do
           @service.refresh_user(nil, nil, nil)
         end
@@ -37,7 +37,7 @@ module RongCloud
         assert_equal "userId is required.", error.message
       end
 
-      def test_refresh_user_with_unexisted_userId
+      def test_refresh_user_with_unexisted_user_id
         error = assert_raises RongCloud::BadRequest do
           @service.refresh_user("user2", nil, nil)
         end
@@ -58,6 +58,24 @@ module RongCloud
       def test_refresh_user_with_chinese_name
         response = @service.refresh_user("user", "小李子", nil)
         assert_equal 200, response["code"]
+      end
+
+      def test_check_online_without_user_id
+        error = assert_raises RongCloud::BadRequest do
+          @service.check_online(nil)
+        end
+        assert_equal "userId is required.", error.message
+        assert_equal 1002, error.business_code
+      end
+
+      def test_check_online_with_unexisted_user_id
+        response = @service.check_online("user2")
+        assert_equal "0", response["status"]
+      end
+
+      def test_check_online_with_existed_user_id
+        response = @service.check_online("user")
+        assert_equal "0", response["status"]
       end
     end
   end
