4717caca1a49aacaf3a8c3c21d2984aff50e8fd7
Martin Hong <hongzeqin@gmail.com>
2016-10-12 08:44:35 +0800

    refactor request to support content type

--- lib/rong_cloud/request.rb
+++ lib/rong_cloud/request.rb
@@ -22,12 +22,20 @@ module RongCloud
     #
     # @param uri [URI] 请求路径对象
     # @param params [Hash] 请求的参数，所有参数通过 form encoded data 方式发送
+    # @param content_type [Symbol] 请求数据编码格式，:form_data 或者 :json
     # @return [Net::HTTP::Post] 请求的实例
     #
-    def initialize_request(uri, params)
+    def initialize_request(uri, params, content_type)
       req = Net::HTTP::Post.new(uri)
-      req.set_form_data(params) if params.respond_to?(:map)
       signed_headers.each { |header, value| req[header] = value }
+
+      case content_type
+      when :form_data
+        req.set_form_data(params) if params.respond_to?(:map)
+      when :json
+        req.body = params.to_json
+        req["Content-Type"] = "application/json"
+      end
 
       req
     end
@@ -53,11 +61,12 @@ module RongCloud
     # 执行请求
     # @param path [String] 请求 API 的相对路径
     # @param params [Hash] 请求的参数
+    # @param content_type [Symbol] 请求数据编码格式，:form_data 或者 :json，默认 :form_data
     # @return [Hash] JSON 解析后的响应数据
     # @raise [RongCloud::BadRequest] 请求参数有误，缺失或者不正确等，详见官方文档
-    def request(path, params = nil)
+    def request(path, params = nil, content_type = :form_data)
       uri = get_uri(path)
-      req = initialize_request(uri, params)
+      req = initialize_request(uri, params, content_type)
       use_ssl = uri.scheme == 'https'
       res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: use_ssl) do |http|
         http.request(req)
