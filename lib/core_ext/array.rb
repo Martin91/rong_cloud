4a7e8bf4692088c4ff7991c8fcfd33f818fee81d
Martin Hong <hongzeqin@gmail.com>
2016-10-18 23:26:54 +0800

    avoid method pollution

--- lib/core_ext/array.rb
+++ lib/core_ext/array.rb
@@ -1,6 +1,8 @@
 class Array
   # inspired by http://api.rubyonrails.org/classes/Array.html#method-i-extract_options-21
-  def extract_options!
-    last.is_a?(Hash) ? pop : {}
+  unless instance_methods.include?(:extract_options!)
+    def extract_options!
+      last.is_a?(Hash) ? pop : {}
+    end
   end
 end
