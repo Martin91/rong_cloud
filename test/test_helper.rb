661d7d0e9454f261694fbea024e1f96b1728de45
Martin Hong <hongzeqin@gmail.com>
2018-03-10 23:36:56 +0800

    set timeout to 10 seconds in test case

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,5 +20,6 @@ def rong_cloud_configure_with_settings
     config.app_key = ENV["RONGCLOUD_APP_KEY"]
     config.app_secret = ENV["RONGCLOUD_APP_SECRET"]
     config.host = ENV["RONGCLOUD_API_HOST"] || "https://api.cn.ronghub.com"
+    config.timeout = 10
   end
 end
