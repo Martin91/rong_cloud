4caf8da6d7ab67737a6934f06e3e6e2aace5b2f4
Martin Hong <hongzeqin@gmail.com>
2018-03-10 22:25:36 +0800

    rename methods

--- lib/rong_cloud/services/message_priority.rb
+++ lib/rong_cloud/services/message_priority.rb
@@ -4,15 +4,15 @@ module RongCloud
   module Services
     # http://www.rongcloud.cn/docs/server.html#chatroom_message_priority
     module MessagePriority
-      def add_chatroom_priority_message(object_name)
+      def add_chatroom_message_priority(object_name)
         request("/chatroom/message/priority/add", objectName: object_name)
       end
 
-      def remove_chatroom_priority_message(object_name)
+      def remove_chatroom_message_priority(object_name)
         request("/chatroom/message/priority/remove", objectName: object_name)
       end
 
-      def chatroom_priority_messages
+      def chatroom_message_priorities
         request("/chatroom/message/priority/query")
       end
     end
--- test/rong_cloud/services/message_priority_test.rb
+++ test/rong_cloud/services/message_priority_test.rb
@@ -5,23 +5,23 @@ module RongCloud
     class MessagePriorityTest < Minitest::Test
       include RongCloud::ServiceTestSetup
 
-      def test_add_priority_message_flow
-        response = @service.add_chatroom_priority_message("RC:CmdMsg")
+      def test_add_message_priority_flow
+        response = @service.add_chatroom_message_priority("RC:CmdMsg")
         assert_equal 200, response["code"]
 
-        response = @service.add_chatroom_priority_message(["RC:DizNtf", "RC:CmdNtf"])
+        response = @service.add_chatroom_message_priority(["RC:DizNtf", "RC:CmdNtf"])
         assert_equal 200, response["code"]
 
-        response = @service.chatroom_priority_messages
+        response = @service.chatroom_message_priorities
         assert_equal ["RC:CmdMsg", "RC:DizNtf", "RC:CmdNtf"].sort, response["objectNames"].sort
 
-        response = @service.remove_chatroom_priority_message(["RC:DizNtf", "RC:CmdNtf"])
+        response = @service.remove_chatroom_message_priority(["RC:DizNtf", "RC:CmdNtf"])
         assert_equal 200, response["code"]
 
-        response = @service.remove_chatroom_priority_message("RC:CmdMsg")
+        response = @service.remove_chatroom_message_priority("RC:CmdMsg")
         assert_equal 200, response["code"]
 
-        response = @service.chatroom_priority_messages
+        response = @service.chatroom_message_priorities
         assert_empty response["objectNames"]
       end
     end
